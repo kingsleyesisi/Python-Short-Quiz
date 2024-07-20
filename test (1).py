bc9f49f8e2026bd13d14efa8fbb60094e9fab7ba
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2024-07-21 00:12:33 +0100

    Add files via upload

---
+++ test (1).py
@@ -0,0 +1,27 @@
+
+
+# import json
+#
+#
+# with open("data.json", 'r') as file:
+#     content = file.read()
+#
+# data = json.loads(content)
+#
+# score = 1
+#
+# for question in data:
+#     print(question['questions_text'])
+#     for index, alternative in enumerate(question['alternatives']):
+#         print(index + 1, "-", alternative)
+#     user_choice = int(input('Enter your answer: '))
+#     question['user_choice'] = user_choice
+#     if question["user_choice"] == question["correct_answer"]:
+#         print('Correct')
+#         score = score + 1
+#
+# for index, question in enumerate(data):
+#     message = (f" your your answer: {question['user_choice']}, "
+#                f"Correct Answer: {question['correct_answer']}")
+#     print(index +1, '-', message)
+# print(f"Your score is {score}/{len(data)}")
